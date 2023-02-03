0b4b6f5e7d48ce5577936f5287d5bb81ca526b9b
Marc Allen David <marcallen.david.18@gmail.com>
2023-02-03 15:17:43 +0800

    Updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,16 +23,11 @@ const registerUser = asyncHandler( async(req, res) => {
         throw new Error("Email has already been registered")
     }
 
-
-    // Hash password before saving to Database
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt)
-
     // Create new user
     const user = await User.create({
         name,
         email,
-        password: hashedPassword 
+        password
     })
 
     if(user){
