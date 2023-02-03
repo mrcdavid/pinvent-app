f1146b48249d748d3dcc65d269c602477a46d812
marcallen.david.18@gmail.com <marcallen.david.18@gmail.com>
2023-02-04 02:32:35 +0800

    Updated userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,63 +1,67 @@
-const asyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET,{expiresIn: "1d"})
+	return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
-const registerUser = asyncHandler( async(req, res) => {
-   const {name, email, password} = req.body;
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
 
-   // Validation
-    if(!name || !email || !password){
-        res.status(400);
-        throw new Error("Please fill in all required fields")
-    }
-    if (password.length < 6){
-        res.status(400);
-        throw new Error("Password must be at least 6 characters")
-    }
+	// Validation
+	if (!name || !email || !password) {
+		res.status(400);
+		throw new Error("Please fill in all required fields");
+	}
+	if (password.length < 6) {
+		res.status(400);
+		throw new Error("Password must be at least 6 characters");
+	}
 
-    // Check if user email already exists
-    const userExists = await User.findOne({email})
-    if(userExists){
-        res.status(400);
-        throw new Error("Email has already been registered")
-    }
+	// Check if user email already exists
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		res.status(400);
+		throw new Error("Email has already been registered");
+	}
 
-    // Create new user
-    const user = await User.create({
-        name,
-        email,
-        password
-    })
+	// Create new user
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
 
-    // Generate token
-    const token = generateToken(user._id)
+	// Generate token
+	const token = generateToken(user._id);
 
-    // Send HTTP-only cookie
-    res.cookie("token", token, {
-        path: "/",
-        httpOnly: true,
-        expires: new Date(Date.now()+ 1000 * 86400),// 1 day
-        sameSite: "none",
-        secure: true
-    });
+	// Send HTTP-only cookie
+	res.cookie("token", token, {
+		path: "/",
+		httpOnly: true,
+		expires: new Date(Date.now() + 1000 * 86400), // 1 day
+		sameSite: "none",
+		secure: true,
+	});
 
-    if(user){
-        const {_id, name, email, photo, phone, bio} = user
-        res.status(201).json({
-            _id, name, email, photo, phone, bio, token
-        })
-    } else {
-        res.status(400);
-        throw new Error("Invalid user data")
-    }
-
-
-})
+	if (user) {
+		const { _id, name, email, photo, phone, bio } = user;
+		res.status(201).json({
+			_id,
+			name,
+			email,
+			photo,
+			phone,
+			bio,
+			token,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid user data");
+	}
+});
 
 module.exports = {
-    registerUser
-}
+	registerUser,
+};
