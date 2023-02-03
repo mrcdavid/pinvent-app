3c4779e59a11136244ad744007a3b1175ad0a659
Marc Allen David <marcallen.david.18@gmail.com>
2023-02-03 16:20:58 +0800

    Updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,13 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET,{expiresIn: "1d"})
+};
 
 const registerUser = asyncHandler( async(req, res) => {
    const {name, email, password} = req.body;
-
 
    // Validation
     if(!name || !email || !password){
@@ -29,10 +33,22 @@ const registerUser = asyncHandler( async(req, res) => {
         password
     })
 
+    // Generate token
+    const token = generateToken(user._id)
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now()+ 1000 * 86400),// 1 day
+        sameSite: "none",
+        secure: true
+    });
+
     if(user){
         const {_id, name, email, photo, phone, bio} = user
         res.status(201).json({
-            _id, name, email, photo, phone, bio
+            _id, name, email, photo, phone, bio, token
         })
     } else {
         res.status(400);
