ac65bd83e38f4c1392c3777ab43ec45736cfeaff
marcallen.david.18@gmail.com <marcallen.david.18@gmail.com>
2023-02-04 12:48:07 +0800

    Updated with userLogout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const generateToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
+// Register User
 const registerUser = asyncHandler(async (req, res) => {
 	const { name, email, password } = req.body;
 
@@ -62,6 +64,79 @@ const registerUser = asyncHandler(async (req, res) => {
 	}
 });
 
+// Login User
+const loginUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	// Validate Request
+	if (!email || !password) {
+		res.status(400);
+		throw new Error("Please add email and password");
+	}
+
+	// Check if user exists
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		res.status(400);
+		throw new Error("User not found, please sign-up");
+	}
+
+	// User exists, check if password is correct
+	const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+	//   Generate Token
+	const token = generateToken(user._id);
+
+	if (passwordIsCorrect) {
+		// Send HTTP-only cookie
+		res.cookie("token", token, {
+			path: "/",
+			httpOnly: true,
+			expires: new Date(Date.now() + 1000 * 86400), // 1 day
+			sameSite: "none",
+			secure: true,
+		});
+	}
+	if (user && passwordIsCorrect) {
+		const { _id, name, email, photo, phone, bio } = user;
+		res.status(200).json({
+			_id,
+			name,
+			email,
+			photo,
+			phone,
+			bio,
+			token,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid email or password");
+	}
+});
+
+// Another method to Logout User
+// const logoutUser = asyncHandler(async (req, res) => {
+// 	res.cookie("token", "", {
+// 		path: "/",
+// 		httpOnly: true,
+// 		expires: new Date(0), // 1 day
+// 		sameSite: "none",
+// 		secure: true,
+// 	});
+// 	return res.status(200).json({ message: "Successfully Logged out" });
+// });
+
+// Logout User
+const logoutUser = asyncHandler(async (req, res) => {
+	res.clearCookie("token");
+	res.status(200).json({
+		message: "Successfully Logged out",
+	});
+});
+
 module.exports = {
 	registerUser,
+	loginUser,
+	logoutUser,
 };
