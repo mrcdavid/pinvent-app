d8fe9a426ce9be294ff5aba31542c34fe295d508
Marc Allen David <marcallen.david.18@gmail.com>
2023-02-06 11:59:52 +0800

    Update userRoute

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser , logoutUser} = require("../controllers/userController");
+const { registerUser, loginUser , logoutUser, getUser} = require("../controllers/userController");
 
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.get("/logout", logoutUser)
+router.get("/getuser", getUser)
+
 
 module.exports = router;
