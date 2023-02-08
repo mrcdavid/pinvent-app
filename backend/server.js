6b9a0e44c9981685a18b5004db8970b9b967a135
Marc Allen David <marcallen.david.18@gmail.com>
2023-02-08 16:36:43 +0800

    Updated

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -19,11 +20,10 @@ app.use(bodyParser.json());
 // Routes Middleware
 app.use("/api/users", userRoute);
 
-
 // Routes
 app.get("/", (req, res) => {
-    res.send("Home Page");
-  });
+  res.send("Home Page");
+});
 
 
 // Error Middleware
@@ -33,11 +33,11 @@ mongoose.set('strictQuery', true);
 const PORT = process.env.PORT || 5000;
 // Connect to DB and start server
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(PORT, () => {
-        
-        })
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+
     })
-    .catch((err) => {})
+  })
+  .catch((err) => { })
 
