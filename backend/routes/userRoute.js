aed9e724468ae3ce313ddb30b7fe8de706a1f594
Marc Allen David <marcallen.david.18@gmail.com>
2023-02-09 16:43:43 +0800

    added loggedin and updateUser routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const { registerUser,
     loginUser,
     logoutUser,
-    getUser,}
+    getUser,
+    loginStatus,
+    updateUser
+    }
     = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 
@@ -14,6 +17,8 @@ router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.get("/logout", logoutUser)
 router.get("/getuser", protect, getUser)
+router.get("/loggedin", loginStatus)
+router.patch("/updateuser", protect, updateUser)
 
 
 module.exports = router;
