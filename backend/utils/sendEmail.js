ee58ec554e7fc994d56348ee6eb7ce4d3e308a92
marcallen.david.18@gmail.com <marcallen.david.18@gmail.com>
2023-02-11 23:43:47 +0800

    Updated

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,36 +1,36 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
+	// Create Email Transporter
+	const transporter = nodemailer.createTransport({
+		host: process.env.EMAIL_HOST,
+		port: 587,
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS,
+		},
+		tls: {
+			rejectUnauthorized: false,
+		},
+	});
 
-    // create email transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: 587,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASS
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
+	// Option for sending email
+	const options = {
+		from: sent_from,
+		to: send_to,
+		replyTo: reply_to,
+		subject: subject,
+		html: message,
+	};
 
-    // Option for sending email
-    const mailOptions = {
-        from: sent_from,
-        to: send_to,
-        subject: subject,
-        html: message
-    }
-
-    // Send email
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(info)
-        }
-    });
+	// send email
+	transporter.sendMail(options, function (err, info) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(info);
+		}
+	});
 };
 
 module.exports = sendEmail;
