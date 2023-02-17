dedf43e7b7cb3c8d8722f28e4dd81cf52e89100b
Marc Allen David <marcallen.david.18@gmail.com>
2023-02-17 15:06:44 +0800

    updated helper.js

--- backend/helper/helper.js
+++ backend/helper/helper.js
@@ -1,3 +1,16 @@
+// /**
+//  * @description Public RSA key to decrypt the encrypted data from the server.
+//  * @type {string}
+//  */
+// const PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----\n${process.env.PUBLIC_KEY}\n-----END PUBLIC KEY-----`;
+
+// /**
+//  * @description Private RSA key to decrypt the encrypted data from the server.
+//  * @type {string}
+//  */
+// const PRIVATE_KEY = `-----BEGIN PRIVATE KEY-----\n${process.env.PRIVATE_KEY}\n-----END PRIVATE KEY-----`;
+// module.exports =  {PUBLIC_KEY, PRIVATE_KEY}
+
 /**
  * @description Public RSA key to decrypt the encrypted data from the server.
  * @type {string}
