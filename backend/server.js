f99f773c2843a520bb10540673a13dcc16019ac1
Marc Allen David <marcallen.david.18@gmail.com>
2023-02-28 13:13:05 +0800

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,13 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 
 // Routes Middleware
 app.use("/api/users", userRoute);
